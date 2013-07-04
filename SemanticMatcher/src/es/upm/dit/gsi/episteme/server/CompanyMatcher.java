f5f471e2e56e9c49c1ad5d64894d972625f84604
Pablo Moncada <pmoncadaisla@gmail.com>
2013-07-04 16:44:47 +0200

    Refactorized code

--- SemanticMatcher/src/es/upm/dit/gsi/episteme/server/CompanyMatcher.java
+++ SemanticMatcher/src/es/upm/dit/gsi/episteme/server/CompanyMatcher.java
@@ -1,3 +1,26 @@
+/*
+* 
+* This file is part of Episteme UI.
+* 
+* Episteme UI has been developed by members of the research Group on 
+* Intelligent Systems [GSI] (Grupo de Sistemas Inteligentes), 
+* acknowledged group by the  Technical University of Madrid [UPM] 
+* (Universidad Politécnica de Madrid) 
+* 
+* Contact: 
+* http://www.gsi.dit.upm.es/;
+* 
+* 
+* Copyright 2012 UPM-GSI: Group of Intelligent Systems - Universidad Politécnica de Madrid (UPM)
+* Licensed under the Apache License, Version 2.0 (the "License"); 
+* You may not use this file except in compliance with the License. 
+* You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by
+* applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific 
+* language governing permissions and limitations under the License.
+* 
+*/
+
 package es.upm.dit.gsi.episteme.server;
 
 import java.io.File;
